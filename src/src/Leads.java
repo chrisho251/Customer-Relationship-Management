37a87f3796c49821c6684c832af6f7371efe7149
chrisho251 <hoduyhoang250196@gmail.com>
2020-09-07 14:48:53 +0700

    Create class
    + Create Main
    + Create class Leads

--- src/src/Leads.java
+++ src/src/Leads.java
@@ -1,7 +1,16 @@
 package src;
 
+import javax.annotation.processing.Generated;
+import java.util.Date;
+
 public class Leads {
 
-    int id;
+    public int id;
+    String name;
+    Date dob;
+    Boolean gender;
+    int phone;
+    String email;
+    String address;
 
 }
